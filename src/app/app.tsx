0e9b91b6d471f3fef78f189790309f7188257e25
Enbi81 <wotng2002@gmail.com>
2023-06-11 00:17:33 +0200

    installing storybook

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,7 @@
 export function App() {
   return (
     <div className={'bg-red-500/10'}>
-      <div className={'text-4xl'}>asad</div>
+      <div className={'text-4xl'}>asadaacc</div>
       <div></div>
     </div>
   );
