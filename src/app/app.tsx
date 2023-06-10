42058850358f1557d0698280d098a00f2694bd4f
Enbi81 <wotng2002@gmail.com>
2023-06-11 00:01:25 +0200

    setting up tailwindcss

---
+++ src/app/app.tsx
@@ -0,0 +1,10 @@
+export function App() {
+  return (
+    <div className={'bg-red-500/10'}>
+      <div className={'text-4xl'}>asad</div>
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
